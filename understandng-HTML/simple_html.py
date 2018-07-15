050d582d2b95b98b359d4b2a5ae4a35037cc75ac
Saurabh Shrivastava <saurabhshrivastava956@gmail.com>
2018-07-15 21:14:21 +0530

    Finding an item in the html page

--- understandng-HTML/simple_html.py
+++ understandng-HTML/simple_html.py
@@ -25,18 +25,9 @@ sample_test = """<!doctype html>
 
   <!-- Add your site or application content here -->
   <p>Hello world! This is HTML5 Boilerplate.</p>
-  <script src="js/vendor/modernizr-{{MODERNIZR_VERSION}}.min.js"></script>
-  <script src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js" integrity="{{JQUERY_SRI_HASH}}" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-{{JQUERY_VERSION}}.min.js"><\/script>')</script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
-
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+  <script src="something1">Something 1</script>
+  <script src="something2">Something 2</script>
+  <script>Nothing</script>
 </body>
 
 </html>"""
@@ -46,4 +37,13 @@ simple_soup = BeautifulSoup(sample_test,"html.parser")
 def find_p():
 	print(simple_soup.find('p').string)
 
-find_p()
+
+def find_script():
+  arr = [scr.string for scr in simple_soup.find_all('script') if "something2" not in scr.attrs.get('src',[])]
+  print(arr)
+
+def find_particular_script():
+  print(simple_soup.find('script',{'src':'something1'}).string)
+
+
+find_particular_script()
